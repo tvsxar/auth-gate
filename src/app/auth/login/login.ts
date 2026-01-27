a80078f296c057fb818ca30aa72fa44232341fe0
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-27 18:34:36 +0200

    feat: implement login functionality

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -7,24 +7,45 @@ import {
   Validators,
 } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../service/auth-service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
   loginForm: FormGroup;
+  loading = false;
+  errorMessage = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+  ) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onSubmit() {
-    console.log(this.loginForm.value);
+    if (this.loginForm.invalid || this.loading) return;
+
+    this.loading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.loading = false;
+        console.log('Logged in')
+      },
+      error: (err) => {
+        this.errorMessage = err?.error?.message || 'Invalid email or password';
+        this.loading = false;
+      },
+    });
   }
 }
