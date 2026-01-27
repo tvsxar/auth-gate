0588460966f07ff6326ef4937668c08b3f31aad4
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-27 18:31:57 +0200

    feat: add login router with credentials

--- src/app/service/auth-service.ts
+++ src/app/service/auth-service.ts
@@ -13,4 +13,10 @@ export class AuthService {
   register(user: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/register`, user);
   }
+
+  login(user: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, user, {
+      withCredentials: true,
+    });
+  }
 }
